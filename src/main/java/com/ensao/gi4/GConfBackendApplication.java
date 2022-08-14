63113caf2bd82e87fd93dc8b5459fccb57825d86
MOUSSA MOHAMED Moubarak <moubarakmoussam@gmail.com>
2022-08-14 16:13:50 +0100

    Initialize project using Spring Starter Project

---
+++ src/main/java/com/ensao/gi4/GConfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ensao.gi4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GConfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GConfBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensao/gi4/GConfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensao.gi4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GConfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
